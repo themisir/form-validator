c30a1c9c60392b28f0c44017d9a5933bb85addfc
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-12-05 18:22:46 +0700

    create i18n support for id (Indonesia)

---
+++ lib/src/i18n/id.dart
@@ -0,0 +1,32 @@
+import '../form_validator_locale.dart';
+
+class LocaleEn extends FormValidatorLocale {
+  @override
+  String name() => 'id';
+
+  @override
+  String minLength(String v, int n) =>
+      'Masukan harus diisi minimum $n karakter';
+
+  @override
+  String maxLength(String v, int n) =>
+      'Masukan harus kurang dari $n karakter';
+
+  @override
+  String email(String v) => 'Masukan bukan email yang benar';
+
+  @override
+  String phoneNumber(String v) => 'Masukan bukan nomor handphone yang benar';
+
+  @override
+  String required() => 'Masukan tidak boleh kosong';
+
+  @override
+  String ip(String v) => 'Masukan bukan alamat IP yang benar';
+
+  @override
+  String ipv6(String v) => 'Masukan bukan alamat IPv6 yang benar';
+
+  @override
+  String url(String v) => 'Masukan bukan alamat URL yang benar';
+}
