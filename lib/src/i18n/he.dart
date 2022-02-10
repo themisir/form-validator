8808c54a6686ed9b3a6ea3e036e23bc910fd9efb
pklreak <78969224+pklreak@users.noreply.github.com>
2022-02-10 13:31:04 +0100

    adding support for Hebrew (#32)
    Adding support for the HE locale for additional language support

---
+++ lib/src/i18n/he.dart
@@ -0,0 +1,34 @@
+import '../form_validator_locale.dart';
+
+class LocaleHe implements FormValidatorLocale {
+  const LocaleHe();
+
+  @override
+  String name() => 'he';
+
+  @override
+  String minLength(String v, int n) =>
+      'השדה חייב להכיל לפחות $n תווים';
+
+  @override
+  String maxLength(String v, int n) =>
+      'השדה חייב להכיל פחות מ- $n תווים';
+
+  @override
+  String email(String v) => 'השדה מכיל כתובת מייל לא תקינה';
+
+  @override
+  String phoneNumber(String v) => 'השדה מכיל מספר טלפון לא תקין';
+
+  @override
+  String required() => 'שדה זה הוא שדה חובה';
+
+  @override
+  String ip(String v) => 'השדה מכיל כתובת IP לא תקינה';
+
+  @override
+  String ipv6(String v) => 'השדה מכיל כתובת IPv6 לא תקינה';
+
+  @override
+  String url(String v) => 'השדה מכיל כתובת URL לא תקינה';
+}
