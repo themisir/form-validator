2d336f9b85a1fdb660fb5eb0deaafc82596bedf2
Sander in 't Hout <sander@inthout.me>
2022-12-15 20:07:54 +0100

    Add Dutch (nl) language translations

---
+++ lib/src/i18n/nl.dart
@@ -0,0 +1,34 @@
+import '../locale.dart';
+
+class LocaleNl implements FormValidatorLocale {
+  const LocaleNl();
+
+  @override
+  String name() => 'nl';
+
+  @override
+  String minLength(String v, int n) =>
+      'Minimaal $n karakters verplicht';
+
+  @override
+  String maxLength(String v, int n) =>
+      'Maximaal $n karakters verplicht';
+
+  @override
+  String email(String v) => 'Ongeldig e-mailadres';
+
+  @override
+  String phoneNumber(String v) => 'Ongeldig telefoonnummer';
+
+  @override
+  String required() => 'Verplicht veld';
+
+  @override
+  String ip(String v) => 'Ongeldig IP-adres';
+
+  @override
+  String ipv6(String v) => 'Ongeldig IPv6-adres';
+
+  @override
+  String url(String v) => 'Ongeldige URL';
+}
